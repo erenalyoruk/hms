53daa4c17fd706b33e887e642bec986b839e2c2b
Eren Alyörük <erenalyoruks@gmail.com>
2022-12-10 00:01:24 +0300

    initial commit

---
+++ src/main/java/net/erenalyoruk/hms/Main.java
@@ -0,0 +1,24 @@
+package net.erenalyoruk.hms;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Label label = new Label("Label");
+        Scene scene = new Scene(new StackPane(label), 800, 600);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
