7e724f42c7dc1c284f50a21374d8e02c4241f141
Eren Alyörük <erenalyoruks@gmail.com>
2022-12-10 14:25:31 +0300

    splitting main and application

--- src/main/java/net/erenalyoruk/hms/Main.java
+++ src/main/java/net/erenalyoruk/hms/Main.java
@@ -1,22 +1,8 @@
 package net.erenalyoruk.hms;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-public class Main extends Application {
-
-    @Override
-    public void start(Stage stage) {
-        Label label = new Label("Label");
-        Scene scene = new Scene(new StackPane(label), 800, 600);
-        stage.setScene(scene);
-        stage.show();
-    }
+public class Main {
 
     public static void main(String[] args) {
-        launch(args);
+        App.start(args);
     }
 }
